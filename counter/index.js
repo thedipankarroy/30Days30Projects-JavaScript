6907eacad1de7e789b9c0dea130157771338c260
Dipankar Roy <dipankarroy1023@gmail.com>
2023-07-04 19:48:01 +0530

    ➕1️ counter done

--- counter/index.js
+++ counter/index.js
@@ -11,20 +11,29 @@ let calculation;
 // FUNCTIONS
 function increaseCount() {
     count.innerText = Number(count.innerText) + 1;
+    colorCount();
 }
 function decreaseCount() {
-    if(count.innerText > 0) {
-        count.innerText = Number(count.innerText) - 1;
-    }
+    count.innerText = Number(count.innerText) - 1;
+    colorCount();
 }
 function resetCount() {
     count.innerText = 0;
+    colorCount();
+}
+
+function colorCount () {
+    Number(count.innerText) > 0 ? count.style.color = "#50be50" :
+    Number(count.innerText) < 0 ? count.style.color = "#be6250" :
+    count.style.color = "#0b1608";
 }
 
 
 // EVENT LISTENER
 document.addEventListener("click", function(event) {
-    event.target.matches("#increase") ? increaseCount() : 
-    event.target.matches("#decrease") ? decreaseCount() : 
-    event.target.matches("#reset") ? resetCount() : null;
+    setTimeout(function () {
+        event.target.matches("#increase") ? increaseCount() : 
+        event.target.matches("#decrease") ? decreaseCount() : 
+        event.target.matches("#reset") ? resetCount() : null;
+    }, 150)
 })
